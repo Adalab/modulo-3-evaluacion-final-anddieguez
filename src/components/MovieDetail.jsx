5db5309c9d2064df3fc2fb04b78d6b62f638db9f
anddieguez <andreadiegar@gmail.com>
2023-10-23 17:42:47 +0200

    added link volver

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -13,12 +13,17 @@ function MovieDetail ({movie}) {
         title={movie.name}
         />
     </a>
+
+    
     <h4 className="card__title">{movie.name}</h4>
     <p className="card__description">{movie.line}</p>
     <p className="card__director">{movie.director}</p>
 
     <a className="card__audio" href={movie.audio} title="Ir al audio" target="_blank" rel="noreferrer"><i className="fa-solid fa-headphones"></i> Escuchar audio</a>
-    
+
+    <div>
+    <Link  to="/">Volver</Link>
+    </div>
 </article>
         
     )
